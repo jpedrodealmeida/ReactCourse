ce7c3db6ef44b5abc9d742aadbd86ff3d585180e
joaos <joaos@DESKTOP-5NRPFBG>
2019-10-20 09:41:13 -0300

    select element on react

--- my-app/src/MyForm.js
+++ my-app/src/MyForm.js
@@ -6,8 +6,24 @@ class MyForm extends Component{
 
         this.state = {
             name: '',
-            message: 'Hello World'
+            message: 'Hello World',
+            fruit: 'orange'
         }
+
+        this.fruits = [
+            {
+                name: 'Apple',
+                value: 'apple'
+            },
+            {
+                name: 'Orange',
+                value: 'orange'
+            },
+            {
+                name: 'Banana',
+                value: 'banana'
+            }
+        ]
         this.handleChange = this.handleChange.bind(this);
     }
 
@@ -21,15 +37,31 @@ class MyForm extends Component{
         const {handleChange} = this;
         return(
             <form>
+                <div>
                 <label>
                     Nome:
                     <input type="text" name="name" value={state.name} onChange={handleChange} /> {state.name}
                     <input type="submit" value="Enviar" />
                 </label>
+                </div>
+                
+                <div>
+                <label>
+                    Fruit:
+                    <select value={state.fruit}>
+                        {this.fruits.map(fruit => <option value={fruit.value}>{fruit.name}</option>)}
+                    </select>
+                </label>
+                </div>
+
+                <div>
                 <label>
                     Message:
                     <textarea value={state.message} />
                 </label>
+                </div>
+                
+                
             </form>
         );
     }
