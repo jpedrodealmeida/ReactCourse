c2218791f7dfd7ceebcdba82dec6aab61e6b0f7e
joaos <joaos@DESKTOP-5NRPFBG>
2019-10-21 19:19:54 -0300

    using api for requests

--- my-app/src/MyForm2.js
+++ my-app/src/MyForm2.js
@@ -4,27 +4,44 @@ class MyForm2 extends Component{
     constructor(props){
         super(props);
         this.initialName = "Nome inicial";
+    
         this.inputName = React.createRef();
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            repos: []
+        }
     }
 
     handleSubmit(event){
-        alert(this.inputName.current.value);
         event.preventDefault();
+        fetch(`https://api.github.com/search/repositories?q=${this.inputName.current.value}`)
+        .then(response => response.json())
+        .then(data => {
+            
+            this.setState({
+                repos: data.items
+            })
+   
+        });
     }
 
     render(){
         const {state} = this;
         return(
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <label>
-                        Nome:
-                    <input defaultValue={this.initialName} type="text" name="name" ref={this.inputName} />
-                    </label>
-                </div>
-                <input type="submit" value="Enviar" />
-            </form>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>
+                            Nome:
+                    <input type="text" name="name" ref={this.inputName} />
+                        </label>
+                    </div>
+                    <input type="submit" value="Enviar" />
+                <ul>
+                    {state.repos.map(item => <li>{item.full_name}</li>)}
+                </ul>
+                </form>
+       
         );
     }
 }
